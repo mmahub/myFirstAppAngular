3773aa38954ee2adc946ab7ccf3e6de7cb2ca396
Muqeeb Personal <muqeebansarinew@gmail.com>
2022-06-27 14:08:37 +0530

    Editing the first App video tutorial o maxomilian angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule   //added FormsModule or two-way binding
   ],
   providers: [],
   bootstrap: [AppComponent]
